3a3fe682556d8de6a44e6852013008bef4f3045a
Ian Granger <icgranger9@gmail.com>
2016-09-08 18:18:14 -0400

    created link to test site

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res, next) {
 	res.sendFile(path.join(__dirname, 'views','index.html'));
 });
 
+app.get('/test', function(req, res, next) {
+	res.sendFile(path.join(__dirname, 'views','test.html'));
+});
+
 app.listen(8080, function () {
 	console.log('server listening on 8080');
 });
