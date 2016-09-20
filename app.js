800a720cf00321e1c28a9250c86c6cd60db7c6a2
icgranger9 <icgranger9@gmail.com>
2016-09-19 20:13:24 -0400

    added public resources to app

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public/resources')));
+
 app.get('/', function(req, res, next) {
 	res.sendFile(path.join(__dirname, 'views/html','index.html'));
 });
