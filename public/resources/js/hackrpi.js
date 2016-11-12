9e27ce172a26400af35189807357e664c1482af7
icgranger9 <icgranger9@gmail.com>
2016-11-12 18:59:06 -0500

    test alternative setup

--- public/resources/js/hackrpi.js
+++ public/resources/js/hackrpi.js
@@ -1,97 +1,90 @@
-	// Your Client ID can be retrieved from your project in the Google
-	// Developer Console, https://console.developers.google.com
-	var CLIENT_ID = '435785755966-rus1t439cnro0968rj9bbkbbt7ltv0q8.apps.googleusercontent.com';
+// Your Client ID can be retrieved from your project in the Google
+// Developer Console, https://console.developers.google.com
+var CLIENT_ID = '435785755966-rus1t439cnro0968rj9bbkbbt7ltv0q8.apps.googleusercontent.com';
 
-	var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 
-	/**
-	* Check if current user has authorized this application.
-	*/
-	function checkAuth() {
-	gapi.auth.authorize(
-		{
-			'client_id': CLIENT_ID,
-			'scope': SCOPES.join(' '),
-			'immediate': true
-		}, handleAuthResult);
+/**
+* Check if current user has authorized this application.
+*/
+function checkAuth() {
+gapi.auth.authorize(
+	{
+		'client_id': CLIENT_ID,
+		'scope': SCOPES.join(' '),
+		'immediate': true
+	}, handleAuthResult);
+}
+
+/**
+* Handle response from authorization server.
+*
+* @param {Object} authResult Authorization result.
+*/
+ function handleAuthResult(authResult) {
+	var authorizeDiv = document.getElementById('authorize-div');
+	if (authResult && !authResult.error) {
+		// Hide auth UI, then load client library.
+		authorizeDiv.style.display = 'none';
+		loadGmailApi();
+	} else {
+		// Show auth UI, allowing the user to initiate authorization by
+		// clicking authorize button.
+		authorizeDiv.style.display = 'inline';
 	}
+}
 
-	/**
-	* Handle response from authorization server.
-	*
-	* @param {Object} authResult Authorization result.
-	*/
-	 function handleAuthResult(authResult) {
-		var authorizeDiv = document.getElementById('authorize-div');
-		if (authResult && !authResult.error) {
-			// Hide auth UI, then load client library.
-			authorizeDiv.style.display = 'none';
-			loadGmailApi();
-		} else {
-			// Show auth UI, allowing the user to initiate authorization by
-			// clicking authorize button.
-			authorizeDiv.style.display = 'inline';
-		}
-	}
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+gapi.auth.authorize(
+	{client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+	handleAuthResult);
+return false;
+}
 
-		/**
-		 * Initiate auth flow in response to user clicking authorize button.
-		 *
-		 * @param {Event} event Button click event.
-		 */
-		function handleAuthClick(event) {
-		gapi.auth.authorize(
-			{client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-			handleAuthResult);
-		return false;
-		}
-
-		/**
-		 * Load Gmail API client library. List labels once client library
-		 * is loaded.
-		 */
-		function loadGmailApi() {
-			gapi.client.load('gmail', 'v1', displayInbox);
-		}
-		
-
-		/**
-		* Print all Labels in the authorized user's inbox. If no labels
-		* are found an appropriate message is printed.
-		*/
-		function displayInbox() {
-		  var request = gapi.client.gmail.users.messages.list({
-			'userId': 'me',
-			'labelIds': 'INBOX',
-			'maxResults': 1000
-		  });
+/**
+ * Load Gmail API client library. List labels once client library
+ * is loaded.
+ */
+function loadGmailApi() {
+	gapi.client.load('gmail', 'v1', listMessages('me', '', listSubjects));
+}
 
 
-			request.execute(function(resp) {
-				var messages = resp.messages;
-				appendPre('Messages:');
+function listMessages(userId, query, callback) {
+	var getPageOfMessages = function(request, result) {
+		request.execute(function(resp) {
+			result = result.concat(resp.messages);
+			var nextPageToken = resp.nextPageToken;
+			if (nextPageToken) {
+				request = gapi.client.gmail.users.messages.list({
+					'userId': userId,
+					'pageToken': nextPageToken,
+					'maxResults': 10, //hardcoded to 10 responses, increase later
+					'q': query
+				});
+				getPageOfMessages(request, result);
+			} else {
+				callback('me', result);
+			}
+		});
+	};
+	var initialRequest = gapi.client.gmail.users.messages.list({
+		'userId': userId,
+		'q': query
+	});
+	getPageOfMessages(initialRequest, []);
+}
 
-				if (messages && messages.length > 0) {
-					for (i = 0; i < messages.length; i++) {
-						var message = messages[i];
-						appendPre( message.payload.headers )
-					}
-				} else { 
-					appendPre('No Labels found.');
-				}
-			});
-		}
 
-
-
-		/**
-		 * Append a pre element to the body containing the given message
-		 * as its text node.
-		 *
-		 * @param {string} message Text to be placed in pre element.
-		 */
-		function appendPre(message) {
-			var pre = document.getElementById('output');
-			var textContent = document.createTextNode(message + '\n');
-			pre.appendChild(textContent);
-		}
+function getMessage(userId, messageId /*, callback*/) {
+	var request = gapi.client.gmail.users.messages.get({
+    	'userId': userId,
+		'id': messageId
+	});
+	request.execute(/*callback*/);
+}
