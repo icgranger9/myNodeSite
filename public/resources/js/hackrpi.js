15ad400b455761015bce66932014a3cac1c93f9b
icgranger9 <icgranger9@gmail.com>
2016-11-12 19:42:38 -0500

    fixed JS?

--- public/resources/js/hackrpi.js
+++ public/resources/js/hackrpi.js
@@ -51,19 +51,35 @@ return false;
  * is loaded.
  */
 function loadGmailApi() {
-	gapi.client.load('gmail', 'v1', listMessages('me', ''));
+	gapi.client.load('gmail', 'v1', function() {
+		listThreads(USER, function(resp) {
+			for(var i = 0; i<5; i++){
+				getThreads(USER, resp["threads"][i]["id"], function(response){
+					console.log(response);
+				});
+				getThreads("me", "THREAD_ID", function (dataMessage) {
+					var temp = dataMessage.messages[0].payload.headers;
+					$.each(temp, function (j, dataItem) {
+						if (dataItem.name == "From") {
+							console.log(dataItem.value);
+						}
+					});
+				});
+			}
+		});
+	});
 }
 
+function listThreads(userId, callback) {
+	var request = gapi.client.gmail.users.threads.list({
+		  'userId': userId
+	});
+	request.execute(callback);
+}
 
-function listMessages (UserId, q, function (dataResult) {
-         $.each(dataResult, function (i, item) {
-           getThread(UserId, item.id, function (dataMessage) {
-             var temp = dataMessage.messages[0].payload.headers;
-             $.each(temp, function (j, dataItem) {
-                   if (dataItem.name == "From") {
-                       Console.log(dataItem.value);
-                    }
-             });
-        });
-    });
-})
+function getThreads(userId, id){
+	var request = gapi.client.gmail.users.threads.get({
+		'userId':userId,
+		'id':id
+	});
+}
